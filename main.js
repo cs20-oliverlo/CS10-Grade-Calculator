ef5313eaf56c22cf8b278554ba38602b7ec18cb3
oliverlo1 <113052541+oliverlo1@users.noreply.github.com>
2022-09-19 10:52:49 -0600

    iomh

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-// JavaScript Code
+// IPO CS10 GRADE CALCULATOR By Oliver Lo
+
+// Input
+let clientSide1 = +prompt("Enter your Client-Side Scripting 1 mark (# only):");
+let sP1 = +prompt("Enter your Structured Programming 1 mark (# only):");
+let sP2 = +prompt("Enter your Structured Programming 2 mark (# only):");
+let clientSide2 = +prompt("Enter your Client-Side Scripting 2 mark (# only):");
+let pA = +prompt("Enter your Project A mark (# only):");
+
+// Process
+let finalGrade = (clientSide1 + sP1 + sP2 + clientSide2 + pA)/5
+
+// Output
+alert(`Your Final CS10 Grade is ${finalGrade}%.`)
